56c4f57cc532ad4981669b78d4eb60063f39db78
pengpeng <forpeng@foxmail.com>
2021-07-20 18:54:33 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {Alert} from 'antd';
 
-function App() {
+const Meditor = (props) => {
+  const {meditorPath, width='100%', height=900, imageUpload=undefined, onSave=undefined, onExport=undefined, onImport=undefined, initValue={}} = props;
+  const [err, setEerr] = useState(null);
+  const onLoad = () => {
+    const minder = window.document.getElementById('meditorFrame')?.contentWindow?.minder;
+    if(!minder) setEerr('初始化编辑器失败, 请检查 meditorPath 配置是否正确!');
+    else {
+      setEerr(null);
+      if(onSave) onSave(minder.exportJson());
+      if(onExport) onExport(minder);
+      if(onImport) onImport(minder);
+      if(initValue) minder.importJson(initValue);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: height, width: width}}>
+      {!err ? <iframe 
+        title="Meditor"
+        id="meditorFrame"
+        src={`${meditorPath}/index.html`}
+        width="100%"
+        height="100%"
+        style={{border: 0}}
+        image-upload={imageUpload}
+        onLoad={onLoad}
+      /> : <Alert type='error' message={err} />}
     </div>
   );
 }
 
-export default App;
+export default Meditor;
