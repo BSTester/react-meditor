7e36576207d1a60de9c0d4af4d5432baa5de921a
Open Stark <forpeng@foxmail.com>
2021-07-21 02:16:00 +0800

    修改minder对象引用方式

--- src/meditor.js
+++ src/meditor.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, forwardRef, useImperativeHandle } from 'react';
 import {Alert} from 'antd';
 
-const Meditor = (props) => {
-  const [err, setEerr] = useState(null);
-  const {meditorPath='/meditor', width='100%', height=900, imageUpload=undefined, getRef=undefined, initValue={}} = props;
+const Meditor = forwardRef((props, ref) => {
+  const [minder, setMinder] = useState(undefined);
+  const [editor, setEditor] = useState(true);
+  const {meditorPath='/meditor', width='100%', height=900, imageUpload=undefined, initValue=undefined} = props;
+  useImperativeHandle(ref, ()=>{
+    return {
+      minder: minder,
+    }
+  }, [minder]);
   const onLoad = () => {
     const minder = window.document.getElementById('meditorFrame')?.contentWindow?.minder;
-    if(!minder) setEerr('初始化编辑器失败, 请检查 meditorPath 配置是否正确!');
-    else {
-      setEerr(null);
-      if(getRef) getRef(minder);
+    if(minder) {
+      setMinder(minder);
+      setEditor(true);
       if(initValue) minder.importJson(initValue);
+    }else{
+      setMinder(undefined);
+      setEditor(false);
     }
   };
   return (
     <div style={{height: height, width: width}}>
-      {!err ? <iframe 
+      {!editor ? 
+        <Alert type='error' style={{textAlign: 'center'}} message='初始化编辑器失败, 请检查 meditorPath 配置是否正确!' /> : 
+      <iframe 
         title="Meditor"
         id="meditorFrame"
         src={`${meditorPath}/index.html`}
@@ -24,9 +34,9 @@ const Meditor = (props) => {
         style={{border: 0}}
         image-upload={imageUpload}
         onLoad={onLoad}
-      /> : <Alert type='error' message={err} />}
+      />}
     </div>
   );
-}
+})
 
 export default Meditor;
